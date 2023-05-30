13fc29ce57b59cb7d601fb101f239a4018b2a475
newage <allonewage@gmail.com>
2023-05-30 13:17:27 +0300

    add menu button

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from keyboards.set_menu import set_main_menu
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
 
@@ -24,6 +25,8 @@ async def main():
     # Инициализируем бот и диспетчер
     bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
+
+    await set_main_menu(bot)
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(user_handlers.router)
--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -21,3 +21,9 @@ LEXICON_RU: dict[str, str] = {
     'user_won': 'Ты победил! Поздравляю!\n\nДавай сыграем еще?',
     'nobody_won': 'Ничья!\n\nПродолжим?',
     'bot_choice': 'Мой выбор'}
+
+LEXICON_COMMANDS_RU: dict[str, str] = {
+                '/start': 'command_1 desription',
+                '/help': 'command_2 desription',
+                '/command_3': 'command_3 desription',
+                '/command_4': 'command_4 desription'}
